8b02bbb4289f21850599ea3ed3a659f67e14c1d1
Nelson <punkpunkada@gmail.com>
2022-02-24 16:00:27 -0300

    criando navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './paginas/Home';
 import Sobre from './paginas/Sobre';
 import PaginaError404 from './paginas/PaginaError404';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <>
       <Router>
+        <Navbar />
         <Switch>
           <Route exact path='/'>
             <Home />
