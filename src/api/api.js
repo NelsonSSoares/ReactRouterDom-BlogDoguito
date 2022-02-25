d4aa70fa21ba1d53ed00431fb75814c0326aa760
Nelson <punkpunkada@gmail.com>
2022-02-25 11:04:59 -0300

    exibindo posts consumindo API

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ export const api = axios.create({
     baseURL: 'http://localhost:5000'
 })
 
-export const busca = async () => {
+export const busca = async (url, setData) => {
     const res = await api.get(url)
     setData(res.data)
 }
