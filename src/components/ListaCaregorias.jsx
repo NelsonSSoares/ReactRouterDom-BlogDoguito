58afde79c685bc911c7bc5263e20f7bae894e959
Nelson <punkpunkada@gmail.com>
2022-03-02 14:01:45 -0300

    criando categorias

---
+++ src/components/ListaCaregorias.jsx
@@ -0,0 +1,30 @@
+import react, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import {busca} from '../api/api'
+import '../assets/css/blog.css'
+
+const ListaCategorias = () =>{
+    
+    const [categorias, setCategorias] = useState([])
+
+    useEffect(()=>{
+        busca('/catogorias', setCategorias)
+    }, [])
+
+    return (
+        <ul className="lista-categorias container flex">
+            {
+                categorias.map((categoria) =>{
+                    <Link to={`categoria/${categoria.id}`}>
+                        <li className={`lista-categorias__categoria lista-categorias__categoria--${categoria.id}`}>
+                            {categoria.nome}
+                        </li>
+                    </Link>
+                    
+                })
+            }
+        </ul>
+    )
+}
+
+export default ListaCategorias;
