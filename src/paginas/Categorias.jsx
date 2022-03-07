04d00c701f319016af0fc4bf689bc3eb7784f88f
Nelson <punkpunkada@gmail.com>
2022-03-07 12:34:22 -0300

    exibindo subcategorias

--- src/paginas/Categorias.jsx
+++ src/paginas/Categorias.jsx
@@ -1,34 +1,57 @@
-import React, { useEffect, useState }  from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, Switch, useParams } from 'react-router-dom';
 import { useRouteMatch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import '../assets/css/blog.css'
 import ListaCategorias from '../components/ListaCaregorias';
 import ListaPost from '../components/ListaPost';
 import { busca } from '../api/api';
+import SubCategoria from './SubCategoria';
 
-const Categoria = () =>{
+const Categoria = () => {
 
-    const {id} = useParams()
-    const { path } = useRouteMatch()
+    const { id } = useParams()
+    const { url, path } = useRouteMatch()
     const [subCategorias, setSubCategoria] = useState([])
 
-    useEffect(()=>{
-        busca(`/categorias/${id}`,(categoria)=>{
+    useEffect(() => {
+        busca(`/categorias/${id}`, (categoria) => {
             setSubCategoria(categoria.subCategorias)
         })
     }, [id])
 
-    return(
+    return (
         <>
             <div className='container'>
                 <h2 className='titulo-pagina'>Pet Noticias</h2>
             </div>
 
-            <ListaCategorias/>
-            <Route exact path={`${path}/`}>
-            <ListaPost url={`/posts?categoria=${id}`}/>
-            </Route>
+            <ListaCategorias />
+
+            <ul className="lista-categorias container flex">
+                {
+                    subCategorias.map((subcategoria) => {
+                        <li className={`lista-categorias__categoria lista-categorias__categoria--${id}`} key={subcategoria}>
+                            <Link to={`${url}/${subcategoria}`}>
+                                {subcategoria}
+                            </Link>
+                        </li>
+                    })
+
+
+                }
+            </ul>
+
+
+            <Switch>
+                <Route exact path={`${path}/`}>
+                    <ListaPost url={`/posts?categoria=${id}`} />
+                </Route>
+
+                <Route path={`${path}/:subcategoria`}>
+                    <SubCategoria />
+                </Route>
+            </Switch>
         </>
     )
 }
